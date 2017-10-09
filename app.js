e1ccfba20c30643de2be4cda5928eba01d8e4e91
Wan Cheul Kim <wckim88@gmail.com>
2017-10-09 09:15:09 -0700

    Check point 2_7 finished

---
+++ app.js
@@ -0,0 +1,29 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', () => {
+    event.preventDefault();
+
+    let title = newToDoText.value;
+
+    let newLi = document.createElement('li');
+
+    let checkbox = documnet.createElement('input');
+
+    checkbox.type = "checkbox";
+
+    newLi.textContent = title;
+
+    newLi.appendChild(checkbox);
+
+    toDoList.appendChild(newLi);
+
+    newToDoText.value = '';
+  });
+}
+
+window.onload = function() {
+  onReady();
+};
